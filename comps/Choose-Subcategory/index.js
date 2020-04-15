fad3edfe338894a89e201c5d8b0deabe36527a67
claudiacastonguay <claudcastonguay@gmail.com>
2020-04-14 17:31:40 -0700

    fixed error

--- comps/Choose-Subcategory/index.js
+++ comps/Choose-Subcategory/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './choose-subcategory.css';
-import Icons from '../Icons';
+import Icon from '../Icon';
 import Menu from '../Menu';
 
 const ChooseSubCategory = () => <div id='choose-subcategory'>
     <div id='flex-icons'>
-        <div><Icons /></div>
+        <div><Icon /></div>
         <div><Menu /></div>
     </div>
 
