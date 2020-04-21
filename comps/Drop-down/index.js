f75b22bea21e1b6bdab51e74887d6968b5c12557
tanishapatel_01@hotmail.co.uk <55719963+tanishap11@users.noreply.github.com>
2020-04-21 11:07:22 -0700

    the topics

--- comps/Drop-down/index.js
+++ comps/Drop-down/index.js
@@ -1,28 +1,11 @@
-import React, { useState } from 'react';
-import { Collapse, Button, CardBody, Card } from 'reactstrap';
+import React from 'react';
 import './dropdown.css';
 
-const DropButton = ({text, props, onClick}) => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggle = () => setIsOpen(!isOpen);
-  
-    return (
-      <div>
-        <Button color="primary" onClick={toggle} style={{ marginBottom: '1rem' }}>Toggle</Button>
-        <Collapse isOpen={isOpen}>
-          <Card>
-            <CardBody>
-            Anim pariatur cliche reprehenderit,
-             enim eiusmod high life accusamus terry richardson ad squid. Nihil
-             anim keffiyeh helvetica, craft beer labore wes anderson cred
-             nesciunt sapiente ea proident.
-            </CardBody>
-          </Card>
-        </Collapse>
-      </div>
-    );  
-}
+const DropButton = ({text, onClick}) => <div onClick={onClick} id='green-button'>
+    <div className='text'>
+        <div className='drop-button'>{text}</div>
+    </div>
+</div>
 
 function DropClick(){
     alert("Default function");
@@ -34,9 +17,3 @@ DropButton.defaultProps = {
 }
 
 export default DropButton;
-
-
-//<div onClick={onClick} id='green-button'>
-//</div>
-
-//    {/* <div className='text'>   <div className='drop-button'>{text}</div>   </div> */}
