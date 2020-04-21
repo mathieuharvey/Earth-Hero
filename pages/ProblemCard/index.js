4ead4af98aa88c52bc6b9cd3640bb5857d10adec
Mathieu Harvey <mathieuharvey@Mathieus-MacBook-Pro.local>
2020-04-21 14:15:17 -0400

    created lesson completed page,added a link to Quiz

--- pages/ProblemCard/index.js
+++ pages/ProblemCard/index.js
@@ -5,6 +5,7 @@ import Menu from '../../comps/Menu';
 import InfoCard from '../../comps/Information Cards';
 
 var CardSideImg = require('../../images/card-side.svg');
+var CardSideImgLeft = require('../../images/card-left.svg');
 
 const ProblemCard = () => <div id="helpcard">
         <Icon />
@@ -14,7 +15,7 @@ const ProblemCard = () => <div id="helpcard">
             text= "Wildfires destroy homes of humans and animals and are also a major health risk. Because of wildfires, many are left without their homes, but also animals who live in the forests. Approximately 800 million animals have died due to wildfires so far. Wildfires also produce smoke that is inhaled by adults and children who live in the affected areas. This is very dangerous, causing breathing problems and even lung damage for many."
             button= "Next"/>
         <img id='card-side-right' src={CardSideImg} />
-        <img id='card-side-left' src={CardSideImg} />
+        <img id='card-side-left' src={CardSideImgLeft} />
         <Menu />
     </div>
 
